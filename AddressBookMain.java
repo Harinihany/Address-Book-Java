d6f9314081752689b3e4ab24d368272a29524f1b
Harinihany <harinihany03@gmail.com>
2020-12-12 07:02:16 +0530

    added: basic details of person like name,city,address

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,4 +1,25 @@
 package com.AddressBook.java;
+class Person 
+{   
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+ 
+    public Person(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+    }    
+}
 
 public class AddressBookMain {
 	public static void main(String[] args) 

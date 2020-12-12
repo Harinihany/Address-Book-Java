2c3b2342c2ad62eefa92e252d8230111a094e747
Harinihany <harinihany03@gmail.com>
2020-12-12 06:55:05 +0530

    added: Welcome message in master branch

---
+++ AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.AddressBook.java;
+
+public class AddressBookMain {
+	public static void main(String[] args) 
+    {
+        System.out.println(" WELCOME TO ADDRESS BOOK PROGRAM");
+    }  
+}
